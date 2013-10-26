1b5c5291e9707d7095a52036967a5703803b4334
Mumo Carlos <mumo.crls@gmail.com>
2013-10-25 22:38:52 -0500

    mmm

--- graph.rb
+++ graph.rb
@@ -2,13 +2,12 @@ require 'ostruct'
 
 class Graph
   attr_accessor :nodes, :initial_node, :current_node, :unvisited_nodes,
-    :tentative, :target_node, :path
+    :tentative, :target_node, :path, :full_path
 
   def initialize args={}
     args.each do |k,v|
       instance_variable_set("@#{k}", v) unless v.nil?
     end
-    @full_path = ''
   end
 
   def current_node
@@ -16,37 +15,51 @@ class Graph
   end
 
   def unvisited_nodes
-    current_node.neighbors
+    current_node.neighbors.keep_if do |node|
+      !@path.keys.include?(node.name)
+    end
   end
 
   def initial_node=(node)
-    @path = [node]
+    @path = { node.name => node }
     @initial_node = node
   end
 
   def get_smaller_neighbor_distance
     unvisited_nodes.each do |node|
-      distance = current_node.distance_to(node)
+      distance = @current_node.distance_to(node)
       if (!@tentative || distance < @tentative.distance)
         @tentative = node
       end
     end
-    puts @tentative.distance
     @current_node = extract_node(@tentative)
-    @path << @current_node
-    #get_smaller_neighbor_distance unless @current_node.name.match @target_node.name
+    @path.merge!({ @current_node.name => @current_node })
+    @tentative = nil
+    get_smaller_neighbor_distance unless @current_node.name.match(@target_node.name)
   end
 
   def shortest_path
     get_smaller_neighbor_distance
+    set_full_path
+    calculate_full_distance
   end
 
   private
   def extract_node(node)
-    puts node.distance
     new_node = @nodes[node.name.to_sym]
     new_node.distance = node.distance
     new_node
   end
 
+  def set_full_path
+    @full_path = @path.keys.join(' -> ')
+  end
+
+  def calculate_full_distance
+    full_distance = 0
+    @path.each do |k, node|
+      full_distance = full_distance + (node.distance || 0) unless  node.name.match(@target_node.name)
+    end
+    full_distance
+  end
 end
